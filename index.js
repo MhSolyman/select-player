ff2189dd0bc0f04f4047e1f49f6e367557739fc5
MhSolyman <solymanbijoye.com>
2022-08-23 04:08:07 +0600

    add total

--- index.js
+++ index.js
@@ -26,18 +26,50 @@ document.getElementById("button-six").addEventListener("click", function () {
 
 
 
+
 let count = 0;
 function myFunction(innerId) {
     count++
-    if(count <=5){ let playerOne = document.getElementById(innerId);
+    if (count <= 5) {
+        let playerOne = document.getElementById(innerId);
         let playerOneText = playerOne.innerText;
         let playerUlList = document.getElementById("ol-list");
         let playerUlListLiList = document.createElement("p");
-        playerUlListLiList.innerHTML = `<h1><span> ${count}<span> ${playerOneText}`
-        playerUlList.appendChild(playerUlListLiList)}
-        else{
-            alert('You can choose only five players')
-        }
-   
+        playerUlListLiList.innerHTML = `<p><span> ${count}<span> ${playerOneText}</p>`
+        playerUlList.appendChild(playerUlListLiList)
+
+        playerAmount(count)
+    }
+    else {
+        alert('You can choose only five players')
+    }
+
+
 }
+function playerAmount(count){document.getElementById('player-doller').addEventListener('click', function () {
+
+    let perplayerValue = document.getElementById("perplayer-value").value;
+    let perplayerValueInt = parseInt(perplayerValue);
+    let amount = document.getElementById('amount');
+    let playerAmount=amount.innerText = perplayerValueInt * count;
+    total(playerAmount)
+
+
+})}
+
+function total(playerAmount){ document.getElementById("total-cal").addEventListener("click",function(){
+    let playerAmountInt=parseInt(playerAmount);
+    let managerValue=document.getElementById("manager").value;
+    let managerValueInt = parseInt(managerValue);
+    let coachVakue=document.getElementById("coach").value;
+    let coachVakueInt=parseInt(coachVakue);
+    let all= playerAmountInt+managerValueInt+coachVakueInt;
+    document.getElementById("total").innerText= all
+
+})
+
+
+}
+
+
 
