c2f8d95517c30afdbdf29c75d28978254c795b55
MhSolyman <solymanbijoye.com>
2022-08-23 01:22:27 +0600

    add function

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-document.getElementById("button-one").addEventListener("click", function () {
-    let playerOne = document.getElementById("Player-one");
-    let playerOneText = playerOne.innerText;
-    let playerUlList = document.getElementById("ol-list");
-    let playerUlListLiList=document.createElement("li");
-    playerUlListLiList.innerText=playerOneText;
-    playerUlList.appendChild(playerUlListLiList)
-    console.log(playerUlListLiList)
 
+document.getElementById("button-one").addEventListener("click", function () {
+  
+
+    
+myFunction("Player-one")
    
 })
-
+let count=0;
+function myFunction(innerId) {
+    count++
+    let playerOne = document.getElementById(innerId);
+    let playerOneText = playerOne.innerText;
+    let playerUlList = document.getElementById("ol-list");
+    let playerUlListLiList=document.createElement("p");
+    playerUlListLiList.innerHTML=`<h1><span> ${count}<span> ${playerOneText}`
+    playerUlList.appendChild(playerUlListLiList)
+  }
 
